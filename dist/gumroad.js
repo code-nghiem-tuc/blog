dba124f6b8fb099d8b75de279fddd1fad71306fe
Minh Thai <minhthai40@gmail.com>
2022-04-12 23:07:18 +0700

    Update gumroad.js

--- dist/gumroad.js
+++ dist/gumroad.js
@@ -1,38 +1,52 @@
+func isHomePage() {
+  return window.location.pathname == '/';
+}
+
+func loadGumroad() {
+  // delete previously added button to avoid duplicate
+  const button = document.querySelector(".gumroad-wrapper");
+  if (button !== null) {
+    console.log("gumroad already loaded");
+    return
+  }
+  
+  const gumroadCode = `
+  <div class="gumroad-wrapper">        
+    <a class="gumroad-button" href="https://codenghiemtuc.gumroad.com/l/buy-me-a-drink">Ủng hộ tui ly cafe</a>
+  </div>
+  `;
+  targetNode.insertAdjacentHTML("beforeend", gumroadCode);
+  console.log("loaded gumroad");
+}
+
+func unloadGumroad() {
+  // delete previously added button
+  const button = document.querySelector(".gumroad-wrapper");
+  if (button !== null) {
+    button.remove();
+    console.log("removed existing gumroad");
+  }
+}
+
 var targetNode = document.querySelector('.super-content');
 if (targetNode == null) {
   console.warn("cannot find Super's content element, fallback to body");
   targetNode = document.body;
 }
 
+// first, let's load if this is not home page
+if (!isHomePage()) {
+  loadGumroad();
+}
+
 var observer = new MutationObserver(function(){
-    if(targetNode.style.display != 'none'){
-      console.log("content display detected");
-      
-      if ( window.location.pathname !== '/' ) { // if not home page
-        // delete previously added button to avoid duplicate
-        const button = document.querySelector(".gumroad-wrapper");
-        if (button !== null) {
-          console.log("gumroad already loaded");
-        } else {
-          const gumroadCode = `
-          <div class="gumroad-wrapper">        
-            <a class="gumroad-button" href="https://codenghiemtuc.gumroad.com/l/buy-me-a-drink">Ủng hộ tui ly cafe</a>
-          </div>
-          `;
-          targetNode.insertAdjacentHTML("beforeend", gumroadCode);
-          console.log("loaded gumroad");
-        }
-      } else {
-        // delete previously added button
-        const button = document.querySelector(".gumroad-wrapper");
-        if (button !== null) {
-          button.remove();
-          console.log("removed existing gumroad");
-        }
-        console.log("this is homepage, skip loading gumroad");
-      }
+    if (!isHomePage()) {
+      loadGumroad();
     } else {
-      console.log("content change detected, but not display");
+      unloadGumroad();
+      console.log("this is homepage, skip loading gumroad");
     }
 });
-observer.observe(targetNode, { attributes: true, childList: true });
+
+// watch for change of 'height'
+observer.observe(targetNode, { attributes: true, attributeFilter: ["height"] });
