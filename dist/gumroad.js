acb4e1560f55158838565d4828980e3bea03abf6
Minh Thai <minhthai40@gmail.com>
2022-04-12 21:55:45 +0700

    Update gumroad.js

--- dist/gumroad.js
+++ dist/gumroad.js
@@ -8,18 +8,23 @@ var observer = new MutationObserver(function(){
     if(targetNode.style.display != 'none'){
       console.log("content display detected");
       
-      // delete previously added button to avoid duplicate
-      const button = document.querySelector(".gumroad-button");
-      if (button !== null) {
-        button.remove();
-        console.log("removed existing gumroad");
-      }
-      
       if ( window.location.pathname !== '/' ) { // if not home page
-        const gumroadCode = '<a class="gumroad-button" href="https://codenghiemtuc.gumroad.com/l/buy-me-a-drink">Tặng mình ly nước</a>';
-        targetNode.insertAdjacentHTML("afterend", gumroadCode);
-        console.log("loaded gumroad");
+        // delete previously added button to avoid duplicate
+        const button = document.querySelector(".gumroad-button");
+        if (button !== null) {
+          console.log("gumroad already loaded");
+        } else {
+          const gumroadCode = '<a class="gumroad-button" href="https://codenghiemtuc.gumroad.com/l/buy-me-a-drink">Tặng mình ly nước</a>';
+          targetNode.insertAdjacentHTML("beforeend", gumroadCode);
+          console.log("loaded gumroad");
+        }
       } else {
+        // delete previously added button
+        const button = document.querySelector(".gumroad-button");
+        if (button !== null) {
+          button.remove();
+          console.log("removed existing gumroad");
+        }
         console.log("this is homepage, skip loading gumroad");
       }
     } else {
