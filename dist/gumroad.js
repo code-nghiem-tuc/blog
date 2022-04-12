a1b813f0d4254222881f2f21a01d4f8e24442706
Minh Thai <minhthai40@gmail.com>
2022-04-12 20:58:10 +0700

    Update gumroad.js

--- dist/gumroad.js
+++ dist/gumroad.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
   if ( window.location.pathname =!= '/' ) { // if not home page
     const gumroadCode = '<a class="gumroad-button" href="https://codenghiemtuc.gumroad.com/l/buy-me-a-drink">Tặng mình ly nước</a>';
-    document.body.insertAdjacentHTML("beforeend", gumroadCode);
+    let content = document.querySelector('.super-content');
+    if (content == null) {
+      console.warn("cannot find Super's content element, fallback to body");
+      content = document.body;
+    }
+    
+    content.insertAdjacentHTML("beforeend", gumroadCode);
+    console.log("loaded gumroad");
+  } else {
+    console.log("not homepage, skip loading gumroad");
   }
 });
