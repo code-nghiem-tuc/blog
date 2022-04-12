f1f8e756ea1c0c8c8a6952a2dd10c1ad8b5ac8a8
Minh Thai <minhthai40@gmail.com>
2022-04-12 20:14:50 +0700

    Update gumroad.js

--- dist/gumroad.js
+++ dist/gumroad.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(){
-  const gumroadCode = '<script src="https://gumroad.com/js/gumroad-embed.js"></script><div class="gumroad-product-embed"><a href="https://codenghiemtuc.gumroad.com/l/buy-me-a-drink">Loading...</a></div>';
+  const gumroadCode = '<div class="gumroad-product-embed"><a href="https://codenghiemtuc.gumroad.com/l/buy-me-a-drink">Loading...</a></div>';
   document.body.insertAdjacentHTML("beforeend", gumroadCode);
 });
