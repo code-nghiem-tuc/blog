5ed39d8dc0f92e1a067b068b59766b98cb264032
Minh Thai <minhthai40@gmail.com>
2022-04-12 21:34:33 +0700

    Update gumroad.js

--- dist/gumroad.js
+++ dist/gumroad.js
@@ -1,15 +1,27 @@
-document.addEventListener("DOMContentLoaded", function(){
-  if ( window.location.pathname !== '/' ) { // if not home page
-    const gumroadCode = '<a class="gumroad-button" href="https://codenghiemtuc.gumroad.com/l/buy-me-a-drink">Tặng mình ly nước</a>';
-    let content = document.querySelector('.super-content');
-    if (content == null) {
-      console.warn("cannot find Super's content element, fallback to body");
-      content = document.body;
+var targetNode = document.querySelector('.super-content');
+if (targetNode == null) {
+  console.warn("cannot find Super's content element, fallback to body");
+  targetNode = document.body;
+}
+
+var observer = new MutationObserver(function(){
+    if(targetNode.style.display != 'none'){
+      console.log("content display detected");
+      
+      if ( window.location.pathname !== '/' ) { // if not home page
+        const button = document.querySelector(".gumroad-button");
+        if button !== null {
+          const gumroadCode = '<a class="gumroad-button" href="https://codenghiemtuc.gumroad.com/l/buy-me-a-drink">Tặng mình ly nước</a>';
+          content.insertAdjacentHTML("beforeend", gumroadCode);
+          console.log("loaded gumroad");
+        } else {
+          console.log("gumroad already loaded, do nothing");
+        }
+      } else {
+        console.log("this is homepage, skip loading gumroad");
+      }
+    } else {
+      console.log("content change detected, but not display");
     }
-    
-    content.insertAdjacentHTML("beforeend", gumroadCode);
-    console.log("loaded gumroad");
-  } else {
-    console.log("this is homepage, skip loading gumroad");
-  }
 });
+observer.observe(targetNode, { attributes: true, childList: true });
