a575ee965098c539952ce6001edf99b9d08484b4
Minh Thai <minhthai40@gmail.com>
2022-04-13 20:35:23 +0700

    Update gumroad.js

--- dist/gumroad.js
+++ dist/gumroad.js
@@ -12,8 +12,20 @@ function loadGumroad() {
   
   const gumroadCode = `
   <div class="gumroad-wrapper">
-    <hr>
-    <a class="gumroad-button" href="https://codenghiemtuc.gumroad.com/l/buy-me-a-drink">Ủng hộ tui ly cafe</a>
+    <div class="donate-container">
+      <div class="spacer"></div>
+      <div class="donate-content">
+        <div>
+          <h1>Ủng hộ tui ly cafe</h1>
+        </div>
+        <div>
+          <a class="gumroad-button" href="https://codenghiemtuc.gumroad.com/l/buy-me-a-drink">Ủng hộ qua </a>
+        </div>
+        <div id="momo-option">
+          <a class="momo-button" href="https://me.momo.vn/codenghiemtuc">Ủng hộ qua <img id="momo-logo" src="https://camo.githubusercontent.com/738540720e25c1dda8a12bad6dc45b40d4d06e51701c4170d392483b0a2ac054/68747470733a2f2f692e696d6775722e636f6d2f416831384d7a312e706e67" alt="MoMo"/></a>
+        </div>
+      </div>
+    </div>
   </div>
   `;
   targetNode.insertAdjacentHTML("beforeend", gumroadCode);
